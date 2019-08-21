db4c1de5496619ab13be2b26db6b188c0aa0daa8
Lyle Scott, III <lyle@ls3.io>
2019-08-21 10:17:23 -0400

    fix branch

--- contrib/jenkins/pyfmt.groovy
+++ contrib/jenkins/pyfmt.groovy
@@ -7,7 +7,7 @@
  */
 def call(String code_dirs) {
     sh "pip3 install --quiet black isort"
-    sh "curl -s -o pyfmt https://raw.githubusercontent.com/GooeeIOT/pyfmt/initial/bin/pyfmt"
+    sh "curl -s -o pyfmt https://raw.githubusercontent.com/GooeeIOT/pyfmt/master/bin/pyfmt"
     sh "chmod +x pyfmt"
     sh "./pyfmt --check ${code_dirs}"
     sh "rm ./pyfmt"
