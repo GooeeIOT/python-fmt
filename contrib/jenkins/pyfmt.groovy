9fe549eb41763b69944dc556fd0811a611e63b92
Lyle Scott, III <lyle@ls3.io>
2019-08-20 23:42:56 -0400

    move to contrib

---
+++ contrib/jenkins/pyfmt.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/groovy
+
+/*
+ * Install and run pyfmt
+ *
+ * code_dirs is a string representing the paths (space delimted) to pass to pyfmt
+ */
+def call(String code_dirs) {
+    sh "pip3 install --quiet black isort"
+    sh "curl -s -o pyfmt https://raw.githubusercontent.com/GooeeIOT/pyfmt/initial/bin/pyfmt"
+    sh "chmod +x pyfmt"
+    sh "./pyfmt --check ${code_dirs}"
+    sh "rm ./pyfmt"
+}
