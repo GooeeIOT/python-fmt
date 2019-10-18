031ba9b4af2797c49e734a62e033d70145a6cc95
Dustin Rohde <dustin.rohde@gmail.com>
2019-10-18 03:58:50 -0400

    add pyfmt.py and requirements file

---
+++ bin/pyfmt.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import shlex
+import sys
+from subprocess import run as run_sh, Popen, PIPE
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="pyfmt",
+    )
+    parser.add_argument(
+        "PATH",
+        default=os.getenv("BASE_CODE_DIR", "."),
+        help="path to base directory where pyfmt will be run;"
+             " defaults to $BASE_CODE_DIR or the current directory",
+    )
+    parser.add_argument(
+        "--check",
+        action="store_true",
+        help="do a dry run and print the files that would be formatted",
+    )
+    parser.add_argument(
+        "--line-length",
+        type=int,
+        default=int(os.getenv("MAX_LINE_LENGTH", 100)),
+        help="wrap lines longer than this value; defaults to $MAX_LINE_LENGTH or 100",
+    )
+    parser.add_argument("--extra-isort-args", default="", help="additional args to pass to isort")
+    parser.add_argument("--extra-black-args", default="", help="additional args to pass to black")
+
+    args = parser.parse_args()
+
+    isort_cmd = f"""
+    isort \\
+        --force-grip-wrap=0 \\
+        --line-width={args.line_length} \\
+        --multi-line=3 \\
+        --use-parentheses \\
+        --recursive \\
+        --trailing-comma \\
+        {args.extra_isort_args} \\
+        {args.PATH}
+    """
+    print(isort_cmd)
+    result = run_sh(shlex.split(isort_cmd), capture_output=True)
+    print(result.stdout.read())
+
+    target_version = f"py{sys.version_info.major}{sys.version_info.minor}"
+    black_cmd = f"""
+    black \\
+        --line-length {args.line_length} \\
+        --target-version {target_version} \\
+        {args.extra_black_args}
+        {args.PATH}
+    """
+    print(black_cmd)
+    result = run_sh(shlex.split(black_cmd), capture_stdout=True)
+    print(result.stdout.read())
+
+
+if __name__ == "__main__":
+    main()
